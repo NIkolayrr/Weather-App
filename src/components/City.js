885ff033d8166c7cd1c79600e3549e01c8adc027
NIkolayrr <niki_r95@yahoo.com>
2017-03-17 00:43:55 +0200

    Added geolocation and sorting of cities by name

--- src/components/City.js
+++ src/components/City.js
@@ -6,10 +6,12 @@ import './City.css';
 class City extends Component {
     constructor(props) {
         super(props);
-        this.state = {data: 'New Data',city: 'New City'};
+        this.state = {data: 'New Data', city: 'New City'};
         this._handleClick = this._handleClick.bind(this);
         this._handleRequest = this._handleRequest.bind(this);
         this._handleError = this._handleError.bind(this);
+        this._getCurrentLocation = this._getCurrentLocation.bind(this);
+        this._showPosition = this._showPosition.bind(this);
     }
 
     _handleClick(event) {
@@ -21,10 +23,11 @@ class City extends Component {
             city: city
         });
 
-        _makeRequest(coordinates.lat,coordinates.lon)
+        _makeRequest(coordinates.lat, coordinates.lon)
             .then(this._handleRequest)
             .catch(this._handleError);
     }
+
     _handleRequest(data) {
         let currentWeather = data["currently"];
 
@@ -32,13 +35,33 @@ class City extends Component {
             data: currentWeather
         });
     }
-    _handleError(error){
+
+    _handleError(error) {
         console.log(error);
+    }
+
+    _getCurrentLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this._showPosition);
+        } else {
+            this._handleError("Geolocation is not supported by this browser.");
+        }
+    }
+
+    _showPosition(position) {
+        _makeRequest(position.coords.latitude, position.coords.longitude)
+            .then(this._handleRequest)
+            .catch(this._handleError);
+
+        this.setState({
+            city: 'The weather for your location'
+        });
+
     }
 
     render() {
         const {cities} = this.props;
-        const cityNames = Object.keys(cities).sort(function(a, b) {
+        const cityNames = Object.keys(cities).sort(function (a, b) {
             return a.localeCompare(b);
         });
         const citiesOptions = [];
@@ -46,10 +69,14 @@ class City extends Component {
             citiesOptions.push(<li onClick={this._handleClick} value={city} key={index}>{city}</li>);
         });
         return (
-            <div>
-                <ul className="cityNav">
-                    {citiesOptions}
-                </ul>
+            <div className="cityContainer">
+                <div className="cityChoice">
+                    <h1>Choose a city</h1>
+                    <button onClick={this._getCurrentLocation}>Current Location</button>
+                    <ul className="cityNav">
+                        {citiesOptions}
+                    </ul>
+                </div>
                 <ViewCity data={this.state.data} city={this.state.city}/>
             </div>
         );
