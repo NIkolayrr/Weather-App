02853d7743442af08d75e7c0359096ee89340587
NIkolayrr <niki_r95@yahoo.com>
2017-03-16 13:55:15 +0200

    update Requester

--- src/Requester.js
+++ src/Requester.js
@@ -1,7 +1,12 @@
-function _makeRequest(url) {
+import $ from 'jquery';
+
+function _makeRequest(lat, lon) {
+    let url = "https://api.darksky.net/forecast/75a865c6e798d8b985688c027348ba0d/" + lat + "," + lon;
     return $.ajax({
         method: "GET",
         url: url,
         dataType: 'jsonp'
     });
 }
+
+export default _makeRequest;
