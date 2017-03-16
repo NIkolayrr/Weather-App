bafa73329b99b7c92a7ac5b1abd6095b912f8fce
NIkolayrr <niki_r95@yahoo.com>
2017-03-17 00:43:14 +0200

    minor change

--- src/components/ChooseCity.js
+++ src/components/ChooseCity.js
@@ -6,7 +6,6 @@ class ChooseCity extends Component{
     render(){
         return(
             <div>
-                <h1>Choose a city</h1>
                 <City cities = { CityJson }/>
             </div>
         )
