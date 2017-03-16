4726bec086ce97fa52d7d2451607ed7ec3f386f3
NIkolayrr <niki_r95@yahoo.com>
2017-03-17 00:44:15 +0200

    updated navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router';
+import './Navigation.css';
 
 class Navigation extends Component {
     render() {
         return (
             <header className="site-header">
-                <h1>Dope weather</h1>
                 <nav>
                     <ul>
-                        <li><Link to="/home">Home</Link></li>
-                        <li><Link to="/about">About</Link></li>
+                        <li className="logo"><Link to="/home" activeClassName="active">Weather-App</Link></li>
+                        <li><Link to="/home" activeClassName="active">Home</Link></li>
+                        <li><Link to="/about" activeClassName="active">About</Link></li>
                     </ul>
                 </nav>
             </header>
